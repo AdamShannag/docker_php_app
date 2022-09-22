4960f8f862f05d5a180d7f80a5a94ac0cad7ae1d
Adam Shannag <shannagadam11@gmail.com>
2022-09-22 14:20:58 +0800

    home view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Course;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', [Course::class, "show"]);
